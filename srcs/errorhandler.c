82b3142bb4a83d568ff8c448d9d86af02b1392da
Othmane Majdoub <omajdoub@c2r1p9.1337.ma>
2023-06-03 22:53:27 +0100

    1337Med

--- srcs/errorhandler.c
+++ srcs/errorhandler.c
@@ -6,7 +6,7 @@
 /*   By: omajdoub <omajdoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 03:44:04 by omajdoub          #+#    #+#             */
-/*   Updated: 2023/05/27 05:27:14 by omajdoub         ###   ########.fr       */
+/*   Updated: 2023/06/03 22:52:56 by omajdoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@ void	errorf(int error)
 	else if (error == 4)
 		ft_putstr_fd("Map not found.\n", 2);
 	else if (error == 5)
-		ft_putstr_fd("No way to our coin", 2);
+		ft_putstr_fd("No way to our coin\n", 2);
 	else if (error == 8)
-		ft_putstr_fd("No way to our Exit", 2);
+		ft_putstr_fd("No way to our Exit\n", 2);
 	else if (error == 6)
 		ft_putstr_fd("Map is not correct.\n", 2);
 	else if (error == 7)
-		ft_putstr_fd("Map must contain at least one P one C and one E.", 2);
+		ft_putstr_fd("Map must contain at least one P one C and one E.\n", 2);
 	else if (error == 9)
-		ft_putstr_fd("Map size", 2);
+		ft_putstr_fd("Map size\n", 2);
 	else if (error == 10)
 	{
 		ft_putstr_fd("You Won.\n", 1);
